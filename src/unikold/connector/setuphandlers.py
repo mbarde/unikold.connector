d368e345e723cfffad1ad2024437802100c59099
Content Management System <cms@uni-koblenz.de>
2019-01-31 15:07:24 +0100

    Make queries and queriesFolder not searchable on install

--- src/unikold/connector/setuphandlers.py
+++ src/unikold/connector/setuphandlers.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
 
@@ -13,9 +14,21 @@ class HiddenProfiles(object):
         ]
 
 
+def makeTypeSearchable(portal, type_id, searchable):
+    blacklisted = list(api.portal.get_registry_record('plone.types_not_searched'))
+    if searchable and type_id in blacklisted:
+        blacklisted.remove(type_id)
+    elif not searchable and type_id not in blacklisted:
+        blacklisted.append(type_id)
+    api.portal.set_registry_record('plone.types_not_searched', tuple(blacklisted))
+
+
 def post_install(context):
-    """Post install script"""
-    # Do something at the end of the installation of this package.
+    portal = api.portal.get()
+    makeTypeSearchable(portal, 'SOAPQuery', searchable=False)
+    makeTypeSearchable(portal, 'LSFQuery', searchable=False)
+    makeTypeSearchable(portal, 'LSFSearchQuery', searchable=False)
+    makeTypeSearchable(portal, 'SOAPQueriesFolder', searchable=False)
 
 
 def uninstall(context):
