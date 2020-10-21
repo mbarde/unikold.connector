d106db5962eed6c613e3da12728f6b5d14bf908c
Content Management System <cms@uni-koblenz.de>
2020-10-21 10:12:28 +0200

    pin zeep version to 3.4.0 to keep python 2.7 support for now

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'Products.GenericSetup>=1.8.2',
         'setuptools',
         'z3c.jbot',
-        'zeep',
+        'zeep==3.4.0',
         'python-ldap',
     ],
     extras_require={
